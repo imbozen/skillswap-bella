ef08f9669db76128ef848dcd668e702f7429cbb5
imbozen <imbozencedarcrest.edu>
2026-02-15 19:12:49 -0500

    Implemented filterSkillsByCategory function

---
+++ skillswap-functions.js
@@ -0,0 +1,9 @@
+function filterSkillsByCategory(skills, category) {
+  if (category === 'All') {
+    return skills;
+  }
+
+  return skills.filter(skill => skill.category === category);
+}
+
+module.exports = { filterSkillsByCategory };
