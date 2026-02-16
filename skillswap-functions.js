a2b7b9b17539312ca371e19909b0dcddb05c8113
imbozen <imbozencedarcrest.edu>
2026-02-15 19:49:47 -0500

    Added matchSkillsToUser implementation and passing tests

---
+++ skillswap-functions.js
@@ -0,0 +1,24 @@
+function filterSkillsByCategory(skills, category) {
+  if (category === 'All') {
+    return skills;
+  }
+
+  return skills.filter(skill => skill.category === category);
+}
+
+function calculateTotalCost(hourlyRate, hours) {
+  return hourlyRate * hours;
+}
+
+function matchSkillsToUser(userNeeds, skills) {
+  return skills.filter(skill =>
+    skill.category === userNeeds.category &&
+    skill.price <= userNeeds.maxPrice
+  );
+}
+
+module.exports = {
+  filterSkillsByCategory,
+  calculateTotalCost,
+  matchSkillsToUser
+};
