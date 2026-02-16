c5752fc958482ac971b94b8b700ce9185f78aaca
imbozen <imbozencedarcrest.edu>
2026-02-15 19:12:20 -0500

    Added failing tests for filterSkillsByCategory

--- skillswap-functions.js
+++
@@ -1,9 +0,0 @@
-function filterSkillsByCategory(skills, category) {
-  if (category === 'All') {
-    return skills;
-  }
-
-  return skills.filter(skill => skill.category === category);
-}
-
-module.exports = { filterSkillsByCategory };
